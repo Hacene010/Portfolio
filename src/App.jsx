4c4bd51c0a65372745d6c329d7852d9d1b41b164
Hacene010 <hamrounehacene06@gmail.com>
2021-07-30 10:48:14 +0200

    created a the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import SampleComponent from './components/SampleComponent';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import About from './components/AboutMe/AboutMe';
+import Career from './components/Career/Career';
+import Achievments from './components/Achievments/Achievments';
+import Contact from './components/Contact/Contact';
+import SApp from './Style';
 
 function App() {
   return (
-    <div className="App">
-      <SampleComponent />
-    </div>
+    <Router>
+      <SApp className="App">
+        <Header />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/career">
+          <Career />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/achievments">
+          <Achievments />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </SApp>
+    </Router>
   );
 }
 
