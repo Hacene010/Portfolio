617dee54b4b0476e57bce22ee2185b0d2047645f
Hacene010 <hamrounehacene06@gmail.com>
2021-08-28 09:15:00 +0200

    reordered the achievments component with a .map

--- src/components/Achievments/Achievments.jsx
+++ src/components/Achievments/Achievments.jsx
@@ -1,111 +1,56 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import SAchievements from './Style';
 
 export default function Achievements() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/projects`).then(({ data }) => {
+      setProjects(data);
+    });
+  }, []);
   return (
-    <SAchievements>
+    <SAchievements id="achievments">
       <div className="container">
         <div className="title-projects">
           <div className="title">
             <h1>Réalisations</h1>
           </div>
           <div className="projects">
-            <div className="github">
-              <div className="pic">
-                <img
-                  src="./img/Achievments/githubStats.png"
-                  alt="githubStats"
-                />
-              </div>
+            {projects.map((project) => {
+              return (
+                <div key={project.id} className="one-project">
+                  <div className="picture">
+                    <img
+                      src={project.imgSrc || null}
+                      alt={project.imgAlt || null}
+                    />
+                  </div>
 
-              <span>
-                <a
-                  href="https://github.com/WildCodeSchool/2103-lille-js-p2-g3-github"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <h2>Projet GithubStats.</h2>
-                </a>
+                  <div className="title-content">
+                    <h2>{project.title || null}</h2>
+                    <p>{project.content || null}</p>
+                  </div>
 
-                <li>
-                  API Github Stats. Application réalisée en 4 semaines en groupe
-                  de 4 wilders. Permettant de recenser des informations sur un
-                  utilisateur de github (développeur) en renseigant son
-                  identifiant.
-                  <br /> Ex : ses repositories, son activité sur Github, ses
-                  technos...
-                </li>
-                <li>Public ciblé : Recruteurs</li>
-              </span>
-            </div>
-
-            <div className="edtech">
-              <div className="pic">
-                <div className="pic">
-                  <img src="/img/Achievments/edTechHdf.png" alt="edtechHdf" />
+                  <div className="btn">
+                    <a
+                      href={project.reposFrontend}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <button type="button">
+                        {project.title}
+                        <img
+                          src="/img/Achievments/github-svg-icon-3.png"
+                          alt="github-icon"
+                        />
+                      </button>
+                    </a>
+                  </div>
                 </div>
-              </div>
-              <span>
-                <h2>Projet EdTech Hauts-de-France</h2>
-                <div className="repos">
-                  <a
-                    href="https://github.com/WildCodeSchool/2103-lille-js-p3-edtech-frontend"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <input type="button" value="Frontend" />
-                  </a>
-
-                  <a
-                    href="https://github.com/WildCodeSchool/2103-lille-js-p3-edtech-backend"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <input type="button" value="Backend" />
-                  </a>
-
-                  <a
-                    href="https://github.com/WildCodeSchool/2103-lille-js-p3-edtech-admin"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <input type="button" value="Admin" />
-                  </a>
-                </div>
-
-                <li>
-                  Projet pour l&apos;association edTechHdf. Site vitrine de
-                  l&apos;association
-                </li>
-                <li>Un site responsive et totalement administrable</li>
-                <li>
-                  Technos et framworks : ReactJs, NodeJs (Express), MySql
-                  (workbench)
-                </li>
-              </span>
-            </div>
-
-            <div className="spaceflix">
-              <img src="/img/Achievments/spaceFlix.png" alt="spaceFlix" />
-              <span>
-                <a
-                  href="https://github.com/Hacene010/spaceflix"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <h2>SpaceFlix</h2>
-                </a>
-
-                <li>
-                  Hackathon de 36h. Réalisation d&apos;une plateforme
-                  ressemblant à netflix pour réduire l&apos;ennui d&apos;un
-                  robot dans l&apos;espace.)
-                </li>
-
-                <li>
-                  Technos et framworks : javaScript, ReactJs, Styled-components
-                </li>
-              </span>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
